272a8eb596f59584e04077864a1cb433e3294490
hecmed <hector.med27@gmail.com>
2019-04-23 20:29:10 -0700

    created class for constructing oath signatures

---
+++ camera/app/src/main/java/com/example/monique/camera/GenerateAPI.java
@@ -0,0 +1,14 @@
+package com.example.monique.camera;
+
+public class GenerateAPI {
+    private String consumerKey = "f1f2b91b71294a4f890c4068cf043d44";
+    // if api uses any of these, be sure that it includes them in this order
+    private String food_id = "";
+    private String method = "";
+    private String nonce = "";
+    private String signature_method = "HMAC-SHA1";
+    private String signature = "";
+    private String timestamp = "";
+    private String version = "1.0";
+    
+}
