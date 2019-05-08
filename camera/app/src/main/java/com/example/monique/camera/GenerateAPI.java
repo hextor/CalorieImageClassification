c9b5d79810dd6bffdebb507a5ab0eea84b9b6b57
Aarib Abdullah <aarib@csu.fullerton.edu>
2019-05-08 12:37:51 -0700

    did it work

---
+++ camera/app/src/main/java/com/example/monique/camera/GenerateAPI.java
@@ -0,0 +1,145 @@
+package com.example.monique.camera;
+
+import android.net.Uri;
+import android.util.Base64;
+import android.util.Log;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/*
+https://platform.fatsecret.com/api/Default.aspx?screen=rapiauth
+creating authentication for api use according to companies design
+ */
+public class GenerateAPI {
+
+
+    private String consumer_key = "f1f2b91b71294a4f890c4068cf043d44";
+    private String private_key = "b5756c64465d41ea99aacd574afbdc95";
+    private String http_method = "GET";
+    private String request_url = "https://platform.fatsecret.com/rest/server.api";
+    // if api uses any of these, be sure that it includes them in this order
+    private String food_id = "";
+    private String format = "json";
+    private String method = ""; // should be food.search or food.get
+    private String nonce = ""; // unique string to be passed
+    private String searchExpression = "";
+    private String signature_method = "HMAC-SHA1";
+    private String signature = "";
+    private String timestamp = "";
+    private String version = "1.0";
+
+    public GenerateAPI() { }
+
+    public String encodeString(String str) {
+
+        String url = "";
+        try{
+            url = Uri.encode(str, "UTF-8").trim();
+        }
+        catch (Exception e){
+            Log.d("Encoding went wrong", e.toString());
+        }
+        return url;
+    }
+
+    public String generateTimestamp(){
+        return Long.toString(System.currentTimeMillis() / 1000 );
+    }
+
+    public String generateSearchAuthParameters() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("format=%s&", this.format));
+        sb.append(String.format("method=%s&", this.method));
+        sb.append(String.format("oauth_consumer_key=%s&", this.consumer_key));
+        sb.append(String.format("oauth_nonce=%s&", this.timestamp+"okay"));
+        sb.append("oauth_signature_method=HMAC-SHA1&");
+        sb.append(String.format("oauth_timestamp=%s&", this.timestamp));
+        sb.append(String.format("oauth_version=%s&", version));
+        sb.append(String.format("search_expression=%s", this.searchExpression));
+        return sb.toString();
+    }
+
+    public String generateGetAuthParameters(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("food_id=%s&", this.food_id));
+        sb.append(String.format("format=%s&", this.format));
+        sb.append(String.format("method=%s&", this.method));
+        sb.append(String.format("oauth_consumer_key=%s&", this.consumer_key));
+        sb.append(String.format("oauth_nonce=%s&", this.timestamp+"okay"));
+        sb.append("oauth_signature_method=HMAC-SHA1&");
+        sb.append(String.format("oauth_timestamp=%s&", this.timestamp));
+        sb.append(String.format("oauth_version=%s", version));
+        return sb.toString();
+    }
+
+    public String generateSignature(){
+        // set up key with private_key + `&`
+        String key = String.format("%s&", this.private_key);
+        // set up base key <HTTP Method>&<Request URL>&<Normalized Parameters>
+        String base = "";
+        if (this.method.compareToIgnoreCase("foods.search") == 0){
+            base = String.format("%s&%s&%s", http_method, encodeString(request_url), encodeString(generateSearchAuthParameters()));
+        }
+        else if (this.method.compareToIgnoreCase("food.get") == 0){
+            base = String.format("%s&%s&%s", http_method, encodeString(request_url), encodeString(generateGetAuthParameters()));
+        }
+
+        // Creating our hmac-sha1 encryption
+        Mac mac;
+        byte[] digest = null;
+        try{
+            mac = Mac.getInstance("HmacSHA1");
+            SecretKeySpec secret = new SecretKeySpec(key.getBytes(), mac.getAlgorithm());
+            mac.init(secret);
+            digest = mac.doFinal(base.getBytes());
+        } catch (Exception e){
+            Log.d("error on key algo: ", e.toString());
+        }
+
+        String result = encodeString(Base64.encodeToString(digest, 0).trim());
+        Log.d("new encoded base signature", result);
+        return result;
+    }
+
+    // now we construct a url for searching for food item by search term
+    public String searchFoodItem(String searchTerm){
+        this.timestamp = generateTimestamp();
+        this.searchExpression = encodeString(searchTerm);
+        this.method = "foods.search";
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s?", this.request_url));
+        sb.append(String.format("format=%s&", this.format));
+        sb.append(String.format("method=%s&", this.method));
+        sb.append(String.format("oauth_consumer_key=%s&", this.consumer_key));
+        sb.append(String.format("oauth_nonce=%s&", this.timestamp + "okay"));
+        sb.append(String.format("oauth_signature=%s&", this.generateSignature()));
+        sb.append(String.format("oauth_signature_method=HMAC-SHA1&"));
+        sb.append(String.format("oauth_timestamp=%s&", this.timestamp));
+        sb.append(String.format("oauth_version=%s&", this.version));
+        sb.append(String.format("search_expression=%s", this.searchExpression)); // encode search term in-case there are spaces
+        Log.d("URL being sent over", sb.toString());
+        return sb.toString();
+    }
+
+    public String getFoodItem(String foodId){
+        StringBuilder sb = new StringBuilder();
+        this.timestamp = generateTimestamp();
+        this.method = "food.get";
+        this.food_id = foodId;
+        sb.append(String.format("%s?", this.request_url));
+        sb.append(String.format("food_id=%s&", this.food_id));
+        sb.append(String.format("format=%s&", this.format));
+        sb.append(String.format("method=%s&", this.method));
+        sb.append(String.format("oauth_consumer_key=%s&", this.consumer_key));
+        sb.append(String.format("oauth_nonce=%s&", this.timestamp + "okay"));
+        sb.append(String.format("oauth_signature=%s&", this.generateSignature()));
+        sb.append(String.format("oauth_signature_method=HMAC-SHA1&"));
+        sb.append(String.format("oauth_timestamp=%s&", this.timestamp));
+        sb.append(String.format("oauth_version=%s", this.version));
+
+        return sb.toString();
+    }
+
+}
---
+++ camera/app/src/main/java/com/example/monique/camera/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.monique.camera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.Iterator;
+
+
+
+public class MainActivity extends AppCompatActivity implements VolleyListener{
+
+    private ImageView mimageView;
+    private static final int REQUEST_IMAGE_CAPTURE= 101;
+    private Classifier classifier;
+    private String mainFoodName = "";
+    private String mainFoodID = "";
+    private String allNutrition = "";
+
+    private Map<String, String> foodVariables = new HashMap<String, String>() {
+        {
+            put("measurement_description", "Measurement Description: ");
+            put("calories", "Calories: ");
+            put("carbohydrate", "Carbs: ");
+            put("protein", "Protein: ");
+            put("fat", "Fat: ");
+            put("saturated_fat", "Saturated Fat: ");
+            put("polyunsaturated_fat", "Polyunsaturated Fat: ");
+            put("monounsaturated_fat", "Monounsaturated Fat: ");
+            put("trans_fat", "Trans Fat: ");
+            put("cholesterol", "Cholesterol: ");
+            put("potassium", "Potassium: ");
+            put("sodium", "Sodium: ");
+            put("fiber", "Fiber: ");
+            put("sugar", "Sugar: ");
+            put("vitamin_a", "Vitamin A: ");
+            put("vitamin_c", "Vitamin C: ");
+            put("calcium", "Calcium: ");
+            put("iron", "Iron: ");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mimageView=findViewById(R.id.imageView);
+        try{
+            classifier = new ClassifierQuantizedMobileNet(this);
+        }
+        catch (Exception e){
+            Log.d("Error on classifier initialization", e.toString());
+        }
+
+    }
+
+
+    public void capture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    public String url;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+            // After image is taken, it will fall into this statement if successful
+            if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+                Bundle extras = data.getExtras();
+                final Bitmap imageBitmap = (Bitmap) extras.get("data");
+                mimageView.setImageBitmap(imageBitmap);
+
+                final List<Classifier.Recognition> results = classifier.recognizeImage(imageBitmap);
+                final TextView foodIDView = findViewById(R.id.foodIdView);
+                final TextView saveFoodID = findViewById(R.id.foodID);
+                // run in debug mode and hover over this line to see data details.
+                Log.d("Results for image", results.toString());  // GET THE FIRST KEYWORD -- results.get(0).getTitle()
+
+                RequestQueue queue = Volley.newRequestQueue(this);
+                GenerateAPI api = new GenerateAPI();
+                url = api.searchFoodItem( results.get(0).getTitle());
+                //url = api.searchFoodItem("apple"); // this handles what the result is (results.get(0).getTitle())
+                Log.d("API URL", url);
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        // if all good you can set the values to a variable or just display the results directly.
+                        Log.d("Response from FatSecret", response.toString());
+                                            try{
+                                                JSONObject res = new JSONObject(response.getJSONObject("foods").getJSONArray("food").get(0).toString());
+                                                saveFoodID.setText(res.getString("food_id"));
+                                                mainFoodName = res.getString("food_name");
+                                                foodIDView.setText(getString(R.string.food_display, res.getString("food_name"), res.getString("food_id")));
+                                                requestFinished(true);
+                                            }
+                                            catch (Exception e){}
+                                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle a generic error with a generic response
+                        Log.d("Error on Volley response", error.toString());
+                        foodIDView.setText("Error on url, try again");
+                    }
+                });
+                queue.add(jsonRequest);
+            }
+        }
+    }
+
+    @Override
+    public void requestFinished(boolean done) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        GenerateAPI api = new GenerateAPI();
+        if (done) {
+            final TextView saveFoodID = findViewById(R.id.foodID);
+            Log.d("error", saveFoodID.getText().toString());
+            final TextView displayResults = findViewById(R.id.calorieTextView);
+            url = api.getFoodItem(saveFoodID.getText().toString());
+            JsonObjectRequest jsonRequest2 = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("Response from FatSecret", response.toString());
+                    /*try {
+                        Log.d("Response", response.getJSONObject("food").getJSONObject("servings").toString());
+                        JSONObject res = new JSONObject(response.getJSONObject("food").getJSONObject("servings").getJSONArray("serving").get(0).toString());
+                        displayResults.setText(getString(R.string.food_results, res.getString("calories")));
+//
+                    }*/
+
+
+                    try {
+                        JSONObject nutrition = new JSONObject(response.getJSONObject("food").getJSONObject("servings").getJSONArray("serving").get(0).toString());
+                        allNutrition = "Food Name: " + mainFoodName + '\n';
+                        Iterator<String> keys = nutrition.keys();
+                        StringBuilder nutritionText = new StringBuilder();
+                        nutritionText.append(allNutrition);
+                        while (keys.hasNext()) {
+                            Object key = keys.next();
+                            // if it finds the key in the map named foodVariables to see if the keys are still left/null
+                            if (foodVariables.containsKey((String) key)) {
+                                //JSONObject value = new JSONObject(nutrition.getJSONObject((String) key).toString());
+                                nutritionText.append(foodVariables.get(key));
+                                nutritionText.append(nutrition.getString((String) key));
+
+                                nutritionText.append('\n');
+                            }
+                        }
+                        allNutrition = nutritionText.toString();
+                        displayResults.setText(allNutrition);
+                        //String allNutrition now has the text all set up to be displayed (theoretically)
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error on Volley response", error.toString());
+                }
+            });
+            Log.d("Error", saveFoodID.getText().toString());
+            Log.d("Error", url);
+            queue.add(jsonRequest2);
+
+        }
+    }
+}
